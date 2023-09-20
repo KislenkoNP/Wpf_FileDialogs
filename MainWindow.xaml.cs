5e448e7fcf9c73a94d9fc0b2d6ba6173b9e58325
KislenkoNP <kis@sibstrin.ru>
2023-09-20 10:30:20 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Wpf_FileDialogs {
+	public partial class MainWindow : Window {
+		public MainWindow () {
+			InitializeComponent ();
+		}
+
+		private void btnOpenFile_Click (object sender, RoutedEventArgs e) {
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+			openFileDialog.InitialDirectory = @"d:\Prog\";
+			if (openFileDialog.ShowDialog () == true)
+				txtEditor.Text = File.ReadAllText (openFileDialog.FileName);
+		}
+		private void btnSaveFile_Click (object sender, RoutedEventArgs e) {
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+			saveFileDialog.InitialDirectory = @"d:\Prog\";
+			if (saveFileDialog.ShowDialog () == true)
+				File.WriteAllText (saveFileDialog.FileName, txtEditor.Text);
+ 		}
+		}
+}
